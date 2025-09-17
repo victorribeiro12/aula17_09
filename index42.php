06e67d18444a67cbe990cb88a69a9440e74d73b9
victorribeiro12 <rcv088156@gmail.com>
2025-09-17 14:41:57 -0300

    Update index42.php

--- index42.php
+++ index42.php
@@ -1,5 +1,10 @@
 <?php
-function calcularAreaTriangulo($base, $altura) {
+
+function areaTriangulo($base, $altura) {
     return ($base * $altura) / 2;
 }
+
+$base = 10;
+$altura = 5;
+echo "A área do triângulo é: " . areaTriangulo($base, $altura) . "\n";
 ?>
